4b0b9810f24d1299ee85fc38ca3e3a6246e761ce
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-09-27 15:31:49 +1000

    adding importable breakpoints for desktop and laptop

---
+++ andrew-gav/src/breakpoints/breakpoints.js
@@ -0,0 +1,15 @@
+const points = {
+  S: '768px',
+  M: '1024px',
+  L: '1440px',
+  XL: '2560px',
+};
+
+const breakpoints = {
+  small: `(min-width: ${points.S})`,
+  medium: `(min-width: ${points.M})`,
+  large: `(min-width: ${points.L})`,
+  extraLarge: `(min-width: ${points.XL})`,
+};
+
+export default breakpoints;
