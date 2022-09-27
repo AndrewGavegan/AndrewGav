8b4710ca250461429c77ff2a1f3ead329c91187a
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-09-27 15:58:54 +1000

    framework

--- andrew-gav/src/App.js
+++ andrew-gav/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
 import Header from '../src/components/aboutme/header'
+import './App.css';
+
+const MainStyle = createGlobalStyle
+  `html, body { margin: 0;}*, *:before, *:after { box-sizing: border-box; }`;
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Header />
+        <MainStyle />
       </React.Fragment>
     );
   }
