fe824eca06412fdaf31bde584c255ab4e32c9bcc
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-10-04 15:23:45 +1100

    using intersection observer hooks

--- andrew-gav/src/App.js
+++ andrew-gav/src/App.js
@@ -1,24 +1,36 @@
-import React, { Component } from 'react'
-import MediaQuery from 'react-responsive';
-import { createGlobalStyle } from 'styled-components';
-import Header from '../src/components/aboutme/header'
+import React from 'react'
 import './App.css';
+import { useInView } from 'react-intersection-observer';
 
-const MainStyle = createGlobalStyle
-  `html, body { margin: 0;}*, *:before, *:after { box-sizing: border-box; }`;
+export default function App() {
+  const { ref: myRef, inView: OnScreen } = useInView()
 
-class App extends Component {
-
-  render() {
-    return (
-      <React.Fragment>
-        <MediaQuery query='(min-device-width: 1224px)'>
-          <Header />
-        </MediaQuery>
-        <MainStyle />
-      </React.Fragment>
-    );
-  }
-}
-
-export default App;
+  // const myRef = useRef();
+  // const [OnScreen, setOnScreen] = useState();
+  // console.log('OnScreen', OnScreen);
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver((entries) => {
+  //     const entry = entries[0];
+  //     setOnScreen(entry.isIntersecting)
+  //   })
+  //   observer.observe(myRef.current)
+  // }, [])
+  return (
+    <div className="Page">
+      <div className="Container">
+        <h1>Andrew Gavegan</h1>
+        <h2 ref={myRef}>Full Stack Web Developer</h2>
+      </div>
+      <div className="Container">
+        <h1>{OnScreen ? 'Nothing' : 'ABOUT ME'}</h1>
+        <p>
+          Full stack developer who thrives working in a diverse team.
+          Determined to always be learning and coding and learning and coding.
+        </p>
+      </div>
+      <div className="Container">
+        <h1>PROJECTS</h1>
+      </div>
+    </div>
+  );
+};
